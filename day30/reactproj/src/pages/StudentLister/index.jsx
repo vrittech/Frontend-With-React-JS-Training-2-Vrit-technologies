dec75c7aa80e4059f5fc966267ea7556d8700934
Nirajan Kunwor <nirajankunwor604@gmail.com>
2023-05-03 17:40:47 +0545

    feat: added edit handle

--- day30/reactproj/src/pages/StudentLister/index.jsx
+++ day30/reactproj/src/pages/StudentLister/index.jsx
@@ -20,6 +20,69 @@ const StudentLister = (props) => {
     setStudent(event.target.value);
   };
 
+  const deleteHandler = (index) => {
+    const filteredStudents = props.students.filter((value, i) => {
+      return i !== index;
+    });
+    props.setStudents(filteredStudents);
+  };
+
+  const StudentItem = ({ stud, index }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedStudent, setEditedStudent] = useState(stud);
+
+    const toggleEdit = () => {
+      setIsEditing(!isEditing);
+    };
+
+    const cancelHandler = () => {
+      setEditedStudent(stud);
+      setIsEditing(false);
+    };
+
+    const changeHandler = (event) => {
+      setEditedStudent(event.target.value);
+    };
+
+    return (
+      <ListGroup.Item className="d-flex justify-content-between">
+        {isEditing ? (
+          <>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              name="editedStudent"
+              value={editedStudent}
+              onChange={(event) => changeHandler(event)}
+            />
+            <span>
+              <Button variant="success">Save</Button>
+              <Button variant="danger" onClick={cancelHandler}>
+                Cancel
+              </Button>
+            </span>
+          </>
+        ) : (
+          <>
+            <h4 className="student">{stud}</h4>
+            <span>
+              <Button onClick={toggleEdit} className="ms-2">
+                <AiFillEdit />
+              </Button>
+              <Button
+                variant="danger"
+                className="ms-2"
+                onClick={() => deleteHandler(index)}
+              >
+                <AiOutlineDelete />
+              </Button>
+            </span>
+          </>
+        )}
+      </ListGroup.Item>
+    );
+  };
+
   return (
     <>
       <h3 className="text-center"> Student Lister</h3>
@@ -29,6 +92,7 @@ const StudentLister = (props) => {
           name="student"
           placeholder="Enter Student Name"
           onChange={changeEventHandler}
+          value={student}
         />
         <Button variant="secondary" onClick={submitHandler}>
           Add Student
@@ -37,22 +101,7 @@ const StudentLister = (props) => {
 
       <ListGroup>
         {props.students.map((stud, index) => {
-          return (
-            <ListGroup.Item
-              key={stud + index}
-              className="d-flex justify-content-between"
-            >
-              <h4 className="student">{stud}</h4>
-              <span>
-                <Button>
-                  <AiFillEdit />
-                </Button>
-                <Button variant="danger" className="ms-2">
-                  <AiOutlineDelete />
-                </Button>
-              </span>
-            </ListGroup.Item>
-          );
+          return <StudentItem key={stud + index} stud={stud} index={index} />;
         })}
       </ListGroup>
     </>
