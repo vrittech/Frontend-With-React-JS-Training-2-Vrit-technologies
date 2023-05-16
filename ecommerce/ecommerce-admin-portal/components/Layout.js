7b1ed78fb93785608fdf8fd5fb2bfd64428a2f61
Nirajan Kunwor <nirajankunwor604@gmail.com>
2023-05-16 17:48:00 +0545

    feat: added sidebar, login, logout

---
+++ ecommerce/ecommerce-admin-portal/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Sidebar'
+
+const Layout = ({ children }) => {
+   return (
+      <>
+         <Nav />
+
+      </>
+   )
+}
+
+export default Layout
--- ecommerce/ecommerce-admin-portal/pages/index.js
+++ ecommerce/ecommerce-admin-portal/pages/index.js
@@ -1,6 +1,20 @@
+import Layout from "@/components/Layout";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Component() {
+  const { data: session } = useSession();
+
   return (
-    <>Ecommerce Admin</>
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>Hello, <b>{session?.user?.name}</b></h2>
+        <div className="flex bg-gray-300 rounded-lg gap-1">
+          <img src={session?.user?.image} className="w-6 h-6" />
+          <span className="px-2">
+            {session?.user?.email}
+          </span>
+        </div>
+      </div>
+    </Layout>
   )
 }
-
