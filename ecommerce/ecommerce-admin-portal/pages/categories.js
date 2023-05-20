a432c17cac8e8e50000bf6f6280586712616f93e
Nirajan Kunwor <nirajankunwor604@gmail.com>
2023-05-20 11:04:49 +0545

    feat: integrated categories edit add

--- ecommerce/ecommerce-admin-portal/pages/categories.js
+++ ecommerce/ecommerce-admin-portal/pages/categories.js
@@ -5,11 +5,11 @@ import React, { useEffect, useState } from 'react'
 const Categories = () => {
    const [name, setName] = useState('');
    const [parentCategory, setParentCategory] = useState('');
+   const [editedCategory, setEditedCategory] = useState(null);
    const [categories, setCategories] = useState([]);
 
    useEffect(() => {
       getCategories();
-      console.log(categories);
    }, [])
 
    const getCategories = async () => {
@@ -17,21 +17,46 @@ const Categories = () => {
       setCategories(resp.data);
    }
 
-   const saveCategory = (e) => {
+   const saveCategory = async (e) => {
       e.preventDefault();
       const data = {
          name,
          parentCategory
       }
-      console.log(data);
+      let resp;
+      if (editedCategory) {
+         data._id = editedCategory._id;
+         resp = await axios.put('/api/categories', data);
+         setEditedCategory(null);
+         setName('');
+         setParentCategory('');
+      } else {
+         resp = await axios.post('/api/categories', data);
+      }
+      if (resp.status === 200) {
+         getCategories();
+         //toaster success service implement
+      } else {
+         //toaster error service implement
+      }
+
    }
+
+   const editCategory = (category) => {
+      setEditedCategory(category);
+      setName(category.name);
+      setParentCategory(category?.parent?._id);
+   }
+
+   const deleteCategory = (category) => { }
+
    return (
       <Layout>
          <h1>Categories</h1>
          <form onSubmit={saveCategory}>
             <div className='flex gap-1'>
-               <input type='text' placeholder='Category name' onChange={(e) => setName(e.target.value)} />
-               <select onChange={(e) => setParentCategory(e.target.value)} >
+               <input type='text' placeholder='Category name' value={name} onChange={(e) => setName(e.target.value)} />
+               <select value={parentCategory} onChange={(e) => setParentCategory(e.target.value)} >
                   <option value="">No parent category</option>
                   {categories.length > 0 &&
                      categories.map((category) => {
@@ -42,6 +67,35 @@ const Categories = () => {
                <button type='submit' className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Save</button>
             </div>
          </form>
+
+         {!editedCategory &&
+            <table className='basic mt-4'>
+               <thead>
+                  <tr>
+                     <td>Category name</td>
+                     <td>Parent  Category</td>
+                     <td></td>
+                  </tr>
+               </thead>
+               <tbody>
+                  {
+                     categories.length > 0 &&
+                     categories.map((category) => {
+                        return <tr key={category._id}>
+                           <td>{category.name}</td>
+                           <td>{category?.parent?.name}</td>
+                           <td>
+                              <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded' onClick={(e) => {
+                                 editCategory(category);
+                              }}>Edit</button>
+                              <button className='bg-red-500 hover:bg-red-700 text-white font-bold px-4 rounded' onClick={(e) => deleteCategory(category)}>Delete</button>
+                           </td>
+                        </tr>
+                     })
+                  }
+               </tbody>
+            </table>
+         }
       </Layout>
    )
 }
